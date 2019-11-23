81920d45b2e0547961af4b2be8f8a57bd8b16495
Eniola Oyewole <2026oyeenid@share.wilsonsd.org>
2019-11-23 21:13:09 +0000

    Created index page

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ fs = require("fs")
 nodemailer = require("nodemailer")
 app = express()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static("public"))
+
+app.get('/',(res,req)=>{
+  fs.createReadStream("public/index.html")
+})
+app.get('/images/:name',(res,req)=>{
+  name = res.params.name
+  fs.exists(`images/${name}`,(exists)=>{
+    if(exists == true){
+      fs.createReadStream(`images/${name}`).pipe(req)
+    }else{
+      req.end("image not found ¯\_(ツ)_/¯")
+    }
+  })
+})
 app.listen(3000,()=>{
   console.log(`Listening on port 3000`)
 })
